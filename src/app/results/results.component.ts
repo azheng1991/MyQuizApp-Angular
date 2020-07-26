54c7a5e5af52edc5f32199c2733c3e5620d3bc55
Allan Zheng <azheng34@outlook.com>
2020-07-26 08:11:56 -0400

    added images

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -9,18 +9,4 @@ import { Answers, Question } from '../quiz.model';
 export class ResultsComponent {
   @Input() answers: Answers;
   @Input() questions: Question;
-
-  answersCheck(incorrectNum: number) {
-    console.log(this.answers);
-    if (incorrectNum === 0) {
-      return 'WOW! You really know your killers. Creep.';
-    }
-    if (incorrectNum <= 1) {
-      return 'You\'re almost a Zodiac level with killer trivia!';
-    }
-    if (incorrectNum <= 3) {
-      return 'I wouldn\'t try to get away with murder anytime soon if I were you.';
-    }
-    if (incorrectNum >= 4) return 'Do you even like true crime?';
-  }
 }
